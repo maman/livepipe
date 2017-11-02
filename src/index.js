aaf88513d0a696b622d6b37eac1c39a0fb8e503e
Achmad Mahardi <achmad@mahardi.me>
2017-11-02 21:23:05 +0800

    Spit out the data on server

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 const express = require('express');
 const {createEngine, createHandler} = require('./lib/Streaming');
 
-const PORT = process.env.PORT;
+const {PORT, RTMP_PORT} = process.env;
 
 const app = express();
-const streamEngine = createEngine(process.env.RTMP_PORT);
+const streamEngine = createEngine(RTMP_PORT);
 
 streamEngine.startServer(client => {
   client.on('connect', (tsId, conInfo) => {
     client.accept(true);
-    console.log('CONNECT', tsId, conInfo);
+    console.log('CLIENT CONNECTED', tsId, conInfo);
   });
 
   client.on('publish', (tsId, cmdObjs, name) => {
-    console.log('PUBLISH', tsId, cmdObjs, name);
+    console.log('CLIENT START PUBLISH', tsId, cmdObjs, name);
+    streamEngine.publish(client, tsId, name || 'TEST_STREAM');
   });
 
-  client.on('play', () => {});
+  client.on('unpublish', () => {
+    console.log('CLIENT UNPUBLISH');
+    streamEngine.unpublish();
+  });
 
   client.enable_av_cb((type, timestamp, isSh, isKey, data) => {
-    console.log('AVCB', type, timestamp, isSh, isKey, data);
+    console.log('DATA SENT BY CLIENT', type, timestamp, data);
+    console.log(
+      'get a %s data. ts:%d, is sequence header:%s, is key frame:%s',
+      type,
+      timestamp,
+      isSh,
+      isKey
+    );
   });
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello worldi!');
+  res.send('Hello world!');
 });
 
 app.listen(PORT, () => {
--- src/lib/Streaming.js
+++ src/lib/Streaming.js
@@ -1,18 +1,32 @@
-const path = require('path');
 const bls = require('bls');
 
 function createEngine(RTMPPort) {
   const config = {
-    log: path.join(__dirname, '../log/streams.log'),
+    log: 'log/rtmpserver.log',
     log_level: 1,
     max_client_num: 20,
     port: RTMPPort,
   };
+  let published = false;
   function startServer(clientHandler) {
     bls.start_server(config, clientHandler);
   }
+  function publish(clientInstance, id, name) {
+    if (!published) {
+      published = true;
+      clientInstance.publish(id, name);
+      clientInstance.publish_stream_name = name;
+    } else {
+      clientInstance.close();
+    }
+  }
+  function unpublish() {
+    published = false;
+  }
   return {
     startServer,
+    publish,
+    unpublish,
   };
 }
 
